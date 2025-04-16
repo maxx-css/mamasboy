8b9026c8928137f50eab97bf9768a0b6259445a0
maxx-css <hellomauriciorivera@gmail.com>
2025-04-15 17:46:55 -0700

    Missed one

--- site-header.js
+++ site-header.js
@@ -2,38 +2,41 @@
 class SiteHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    <header class="site-header">
-      <div class="header-content">
-        <div class="logo">
-          <img src="images/mamasboy.jpeg" alt="Barbershop Logo" />
-        </div>
-      </div>
-      <div id="menuToggle" class="mobile-menu">
-        <input type="checkbox" />
-        <span></span>
-        <span></span>
-        <span></span>
+<header class="site-header">
+  <div class="header-content">
+    <div class="logo"> Hello
+    </div>
 
-        <ul id="menu">
-          <li>
-            <a href="index.html" class="nav-link">Home</a>
-          </li>
-          <li>
-            <a href="index.html#about" class="nav-link">Contact</a>
-          </li>
-          <li><a href="contact.html" class="nav-link">Book Now</a></li>
-        </ul>
-      </div>
+    <div id="menuToggle" class="mobile-menu">
+      <input type="checkbox" />
+      <span></span>
+      <span></span>
+      <span></span>
 
-      <!-- Navigation Bar -->
-      <nav class="desktop-navigation navbar nav-links">
-        <ul>
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#contact">Contact</a></li>
-          <li><a href="#contact" class="book-now">Book Now</a></li>
-        </ul>
-      </nav>
-    </header>
+      <ul id="menu">
+        <li>
+          <a href="index.html" class="nav-link">Home</a>
+        </li>
+        <li>
+          <a href="index.html#about" class="nav-link">About</a>
+        </li>
+        <li>
+          <a href="index.html#work" class="nav-link">Work</a>
+        </li>
+        <li><a href="contact.html" class="nav-link book-now">Book Now</a></li>
+      </ul>
+    </div>
+
+    <nav class="desktop-navigation">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="index.html#about">About</a></li>
+        <li><a href="index.html#work">Work</a></li>
+        <li><a href="contact.html" class="book-now">Book Now</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
     `;
     const menuCheckbox = this.querySelector('#menuToggle input');
     menuCheckbox.addEventListener('change', () => {
